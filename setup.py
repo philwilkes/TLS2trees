13f565c2a0a2cf18cd1943cfb242ac77d7b21369
Daniel Lusk <daniel.lusk@posteo.net>
2022-11-17 17:47:52 +0100

    add setup for local module packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='fsct',
+    version='0.1',
+    description='Classify and segment trees from 3D point clouds',
+    url='https://github.com/philwilkes/rxp-pipeline',
+    author='Phil Wilkes',
+    author_email='p.wilkes@ucl.ac.uk',
+    license='Unlicense',
+    packages=['fsct'],
+    zip_safe=False
+)
+
